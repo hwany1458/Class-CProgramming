515d0816dbf9d1fec263308dcc8e7c1253d8a590
hwany1458 <hwany1458@empal.com>
2022-11-05 02:06:35 +0900

    sync

--- 생능-명품C/예제/예제02-1.cpp
+++ 생능-명품C/예제/예제02-1.cpp
@@ -1,5 +1,5 @@
 #include  <stdio.h>
 void main()
 {
-    printf("C¾ð¾î programming");
+    printf("C?? programming");
 }
--- 생능-명품C/예제/예제02-2.cpp
+++ 생능-명품C/예제/예제02-2.cpp
@@ -1,5 +1,5 @@
 #include  <stdio.h>
 void main()
 {
-    printf("C¾ð¾î programming\n");
+    printf("C?? programming\n");
 }
