cd5827e6950492e4224968c11adfd690e47f8ead
hwany1458 <hwany1458@empal.com>
2026-03-18 08:23:21 +0900

    Update hello.c

--- InClass2026/hello.c
+++ InClass2026/hello.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
-void main() {
-        printf("Hello World!");
+// C언어 기본 구조
+void main()
+{
+	// 한줄 주석 - 컴파일러에서 번역(컴파일) 하지 않음......
+	printf("Hello YongHwan");
+	// 자동완성 기능을 활용하세요.
+
+	/*
+	여러줄 주석
+	(칼라로 구분되어 보임)
+	
+	*/
 }
